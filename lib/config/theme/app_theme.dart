0a174615e7cffd4d6f1d06b8a630725ef4ca1e87
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-14 13:54:57 -0600

    Setting primary font and elevated button theme

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,13 +3,26 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
 
+  // TEXT
+  static const String prmFont = 'Poppins';
+
   // COLOR BASE
   static const Color kSeedColor = Color(0xFF49BDFE);
+
+  // COLORS VARIETY
+  static const Color kWhite = Color.fromARGB(255, 247, 244, 244);
 
   ThemeData getTheme() {
     return ThemeData(
       useMaterial3: true,
       colorSchemeSeed: kSeedColor,
+      fontFamily: prmFont,
+      elevatedButtonTheme: const ElevatedButtonThemeData(
+        style: ButtonStyle(
+          backgroundColor: WidgetStatePropertyAll(kSeedColor),
+          foregroundColor: WidgetStatePropertyAll(kWhite)
+        )
+      )
     );
   }
 
