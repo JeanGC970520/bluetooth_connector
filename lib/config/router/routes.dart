b1000f32cf094ed2dea186ca51614b58146ad28d
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-14 17:22:26 -0600

    Adding dashboard screen route

--- lib/config/router/routes.dart
+++ lib/config/router/routes.dart
@@ -4,5 +4,6 @@ import 'package:bluetooth_connector/domain/entities/route.dart';
 class Routes {
 
   static Route home = Route(name: 'home', path: '/');
+  static Route dashboard = Route(name: 'dashboard', path: '/dashboard');
 
 }
