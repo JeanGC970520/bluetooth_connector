b1acac1060d4bef9f3953f6e03683a382098f077
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-25 14:52:44 -0600

    Exporting general dialog

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,4 +1,5 @@
 
 export 'package:bluetooth_connector/presentation/widgets/ble_scanner/circle.dart';
+export 'package:bluetooth_connector/presentation/widgets/shared/general_dialog.dart';
 export 'package:bluetooth_connector/presentation/widgets/ble_scanner/bluetooth_scanner.dart';
 export 'package:bluetooth_connector/presentation/widgets/ble_scanner/bluetooth_device_icon.dart';
