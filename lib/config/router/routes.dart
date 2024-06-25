63c2d36886802a8c954028734f99799a6bbfa564
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-25 10:41:29 -0600

    Adding view routes

--- lib/config/router/routes.dart
+++ lib/config/router/routes.dart
@@ -4,6 +4,7 @@ import 'package:bluetooth_connector/domain/entities/route.dart';
 class Routes {
 
   static Route home = Route(name: 'home', path: '/');
-  static Route dashboard = Route(name: 'dashboard', path: '/dashboard');
+  static Route search = Route(name: 'search', path: '/search');
+  static Route chat = Route(name: 'chat', path: '/chat');
 
 }
