7b790b248bfbe9c313a596c1cea1624ec6626e93
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-25 11:23:26 -0600

    Exporting bluetooth scanner

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,3 +1,4 @@
 
 export 'package:bluetooth_connector/presentation/widgets/ble_scanner/circle.dart';
+export 'package:bluetooth_connector/presentation/widgets/ble_scanner/bluetooth_scanner.dart';
 export 'package:bluetooth_connector/presentation/widgets/ble_scanner/bluetooth_device_icon.dart';
