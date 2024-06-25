7587c3545f0c19d5ad616d43106ec52f3bd4f557
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-25 10:42:19 -0600

    Using ShellRoute to mantein appbar and bottom nav bar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,14 +1,22 @@
 
+import 'package:flutter/cupertino.dart';
+
 import 'package:go_router/go_router.dart';
 
 import 'package:bluetooth_connector/config/router/routes.dart';
+import 'package:bluetooth_connector/presentation/views/views.dart';
 import 'package:bluetooth_connector/presentation/screens/screens.dart';
 
+final _rootNavigatorKey = GlobalKey<NavigatorState>();
+final _shellNavigatorKey = GlobalKey<NavigatorState>();
+
 final appRouter = GoRouter(
+  navigatorKey: _rootNavigatorKey,
   initialLocation: Routes.home.path,
   routes: [
     
     GoRoute(
+      parentNavigatorKey: _rootNavigatorKey,
       name: Routes.home.name,
       path: Routes.home.path,
       builder: (context, state) {
@@ -16,12 +24,37 @@ final appRouter = GoRouter(
       },
     ),
 
-    GoRoute(
-      name: Routes.dashboard.name,
-      path: Routes.dashboard.path,
-      builder: (context, state) {
-        return const DashboardScreen();
+    ShellRoute(
+      parentNavigatorKey: _rootNavigatorKey,
+      navigatorKey: _shellNavigatorKey,
+      builder: (context, state, child) {
+        final String title = state.fullPath == Routes.search.path ? 'Search device' : 'Chat';
+        return DashboardScreen(
+          title: title,
+          child: child,
+        );
       },
+      routes: [
+
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: Routes.search.path,
+          name: Routes.search.name,
+          builder: (context, state) {
+            return const SearchDevicesView();
+          },          
+        ),
+
+        GoRoute(
+          parentNavigatorKey: _shellNavigatorKey,
+          path: Routes.chat.path,
+          name: Routes.chat.name,
+          builder: (context, state) {
+            return const ChatView();
+          },
+        ), 
+
+      ],
     ),
 
   ]
