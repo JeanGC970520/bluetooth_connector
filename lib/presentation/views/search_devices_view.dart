6c6e44f9046dabda2516a923fe70f44593ccb691
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-25 10:40:56 -0600

    Adding views to search devices and chat with them

---
+++ lib/presentation/views/search_devices_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchDevicesView extends StatelessWidget {
+   
+  const SearchDevicesView({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('SearchDevicesView'),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/views/views.dart
@@ -0,0 +1,4 @@
+
+
+export 'package:bluetooth_connector/presentation/views/chat_view.dart';
+export 'package:bluetooth_connector/presentation/views/search_devices_view.dart';
