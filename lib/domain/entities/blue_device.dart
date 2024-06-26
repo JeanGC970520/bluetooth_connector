32a2cb4b6af3820623317bea4dac5842d54be135
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-26 16:42:00 -0600

    Implementing method to select image and color

--- lib/domain/entities/blue_device.dart
+++ lib/domain/entities/blue_device.dart
@@ -1,11 +1,14 @@
 
 
+import 'package:flutter/material.dart';
+import 'package:bluetooth_connector/config/theme/app_theme.dart';
+
 class BlueDevice {
 
   final String name;
   final String id;
   final String? rssi;
-  final int? deviceClass; // https://domoticx.com/bluetooth-class-of-device-lijst-cod/ https://stackoverflow.com/questions/29513987/determine-type-of-connected-bluetooth-device-in-android
+  final int? deviceClass; // https://domoticx.com/bluetooth-class-of-device-lijst-cod/ https://stackoverflow.com/questions/29513987/determine-type-of-connected-bluetooth-device-in-android https://stackoverflow.com/questions/70535290/bluetoothclass-of-devices-what-is-what
 
   BlueDevice({
     required this.name, 
@@ -13,6 +16,21 @@ class BlueDevice {
     required this.rssi,
     required this.deviceClass,
   });
+
+  (String?, Color) getImageAndColor() {
+    switch (deviceClass) {
+      case 0x0540: // Keyboard
+        return ('assets/images/keyboard.png',AppTheme.kYellow);
+      case 0x0580: // Mouse - 1408
+        return ('assets/images/mouse.png',AppTheme.kPurple);
+      case 0x0200: // Phone
+        return ('assets/images/mobile.png',AppTheme.kOrange);
+      case 0x0400: // Audio/video
+        return ('assets/images/airpods.png',AppTheme.kGreen);
+      default: // Other.
+        return (null, AppTheme.kSeedColor);
+    }
+  }
 
   @override
   String toString() {
