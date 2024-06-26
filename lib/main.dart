7fbd51893df47fff1dc703908f9cc1430f67cb3f
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-26 14:30:15 -0600

    Adding repository provider

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bluetooth_connector/config/theme/app_theme.dart';
 import 'package:bluetooth_connector/config/router/app_router.dart';
 import 'package:bluetooth_connector/presentation/blocs/bloc/bluetooth_bloc.dart';
+import 'package:bluetooth_connector/infrastructure/datasource/blue_plus_datasource.dart';
+import 'package:bluetooth_connector/infrastructure/repositories/blue_repository_impl.dart';
 
 void main() {
-  runApp(MultiBlocProvider(
-    providers: [
-      BlocProvider(
-        create: (context) => BluetoothBloc(),
-      ),
-    ],
+  runApp(RepositoryProvider(
+    create: (context) => BlueRepositoryImpl(BluePlusDatasource()),
     child: const MyApp(),
   ));
 }
@@ -23,11 +21,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Bluetooth connector',
-      debugShowCheckedModeBanner: false,
-      routerConfig: appRouter,
-      theme: AppTheme().getTheme(),
+    return BlocProvider(
+      create: (context) => BluetoothBloc(context.read<BlueRepositoryImpl>()),
+      child: MaterialApp.router(
+        title: 'Bluetooth connector',
+        debugShowCheckedModeBanner: false,
+        routerConfig: appRouter,
+        theme: AppTheme().getTheme(),
+      ),
     );
   }
 }
