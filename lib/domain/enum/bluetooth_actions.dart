510e40f2f144b517ee22c15ee33f0bf53f29a52f
Jean Garcia <jeanpoolgarcia_97@hotmail.com>
2024-06-28 11:23:23 -0600

    Adding new values on bluetooth actions enum

--- lib/domain/enum/bluetooth_actions.dart
+++ lib/domain/enum/bluetooth_actions.dart
@@ -4,9 +4,13 @@
 enum BluetoothActions {
   initial,
   scanning,
-  scanned;
+  scanned,
+  connecting,
+  connected;
 
   bool get isInitial => this == BluetoothActions.initial;
   bool get isScanning => this == BluetoothActions.scanning;
   bool get isScanned => this == BluetoothActions.scanned;
+  bool get isConnecting => this == BluetoothActions.connecting;
+  bool get isConnected => this == BluetoothActions.connected;
 }
